fa9eeb1904521689975d990600586914c962def7
vantonytran <vantonytran97@gmail.com>
2018-04-03 11:14:17 +0200

    added Testaufgabe folder

---
+++ Testaufgabe/Testaufgabe/TestaufgabePage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace Testaufgabe
+{
+    public partial class TestaufgabePage : ContentPage
+    {
+        public TestaufgabePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
